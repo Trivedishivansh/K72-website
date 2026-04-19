35b819045593a66635d9debebdc48256f3bd6078
Trivedi Shivansh <shivanahtrivedi@gmail.com>
2026-04-19 11:59:04 +0530

    Add files via upload

---
+++ post sql.sql
@@ -0,0 +1,30 @@
+-- Create Database db_02;
+
+-- Create Table Students(
+-- student_id INT,
+-- name char(50),
+-- age INT,
+-- grade char(1)
+
+
+
+-- );
+-- Insert Into Students(name,age,grade)
+-- values('shivansh',21,'A'),
+--        ('vandan',22,'B'),
+-- 	   ('krish',22,'c')
+-- select * From Students;
+
+-- select name from Students where age>21;
+
+
+-- Update Students
+-- SET age = 23
+-- where name = 'shivansh';
+
+-- select * From students
+
+Delete from students
+where name = 'krish'
+
+select * From students	
